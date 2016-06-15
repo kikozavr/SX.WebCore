c14bad08d87e57b49acb0805ea7a35c064631401
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-06-16 02:30:47 +0300

    release-0

---
+++ SX.Web.Core/MvcConfig/SxMvcRouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SX.Web.Core.MvcConfig
+{
+    public abstract class SxMvcRouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, area="" }
+            );
+        }
+    }
+}
---
+++ SX.Web.Core/MvcControllers/SxBaseMvcController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+
+namespace SX.Web.Core.MvcControllers
+{
+    public abstract class SxBaseMvcController : Controller
+    {
+        private string _areaName;
+        private string _controllerName;
+        private string _actionName;
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var routeDataValues = filterContext.RequestContext.RouteData.Values;
+            _areaName = (string)routeDataValues["area"];
+            _controllerName = (string)routeDataValues["controller"];
+            _actionName = (string)routeDataValues["action"];
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        protected string SxAreaName
+        {
+            get
+            {
+                return _areaName;
+            }
+        }
+
+        protected string SxControllerName
+        {
+            get
+            {
+                return _controllerName;
+            }
+        }
+
+        protected string SxActionName
+        {
+            get
+            {
+                return _actionName;
+            }
+        }
+    }
+}

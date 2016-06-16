bf27969fd225050a186721c8bc550895e7f2cffd
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-06-16 09:39:34 +0300

    release-0

---
+++ SX.WebCore/Repositories/RepoVideo.cs
@@ -0,0 +1,10 @@
+﻿using SX.WebCore.Abstract;
+using System;
+
+namespace SX.WebCore.Repositories
+{
+    public sealed class RepoVideo<TDbContext> : SxDbRepository<Guid, SxVideo, TDbContext> where TDbContext : SxDbContext
+    {
+        
+    }
+}
---
+++ SX.WebCore/SxManual.cs
@@ -0,0 +1,11 @@
+﻿using SX.WebCore.Abstract;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SX.WebCore
+{
+    [Table("D_MANUAL")]
+    public class SxManual : SxMaterial
+    {
+        
+    }
+}
